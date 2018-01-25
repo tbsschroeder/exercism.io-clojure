3dfa11ba01a04e7869ce033b706d92632452756a
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2018-01-25 07:46:50 +0100

    Refactor

--- isbn-verifier/src/isbn_verifier.clj
+++ isbn-verifier/src/isbn_verifier.clj
@@ -1,23 +1,22 @@
 (ns isbn-verifier)
 (use '[clojure.string :as str])
 
-(defn digits [number] (map #(Character/digit % 10) (str number)))
-
-(defn isbn_bak?
+(defn isbn-str-to-digits
       [isbn]
-      (if (= 10 (count (str/replace isbn "-" "")))
-        (if (= "X" (nth (str/replace isbn "-" "") 9))
-          (= 0 (mod (+ 10 (reduce + (map * (range 10 1 -1) (digits (read-string (join "" (drop-last (str/replace isbn "-" "")))))))) 11))
-          (= 0 (mod (reduce + (map * (range 10 0 -1) (digits (read-string (str/replace isbn "-" ""))))) 11)))))
+      (map #(Character/digit % 10) (str (read-string (str/replace isbn "-" "")))))
+
+(defn isbn-x-str-to-digits
+      [isbn]
+      (map #(Character/digit % 10) (str (read-string (join "" (drop-last (str/replace isbn "-" "")))))))
+
 
 
 (defn isbn?
       [isbn]
-      (if (re-matches #"[0-9]\-?[0-9]{3}\-?[0-9]{5}\-?[0-9]" isbn)
-          (= 0 (mod (reduce + (map * (range 10 0 -1) (digits (read-string (str/replace isbn "-" ""))))) 11))
-          (if (re-matches #"[0-9]\-?[0-9]{3}\-?[0-9]{5}\-?X" isbn)
-              (= 0 (mod (+ 10 (reduce + (map * (range 10 1 -1)
-                                               (digits (read-string (join "" (drop-last (str/replace isbn "-" "")))))))) 11))
+      (if (re-matches #"\d\-?\d{3}\-?\d{5}\-?\d" isbn)
+          (= 0 (mod (reduce + (map * (range 10 0 -1) (isbn-str-to-digits isbn))) 11))
+          (if (re-matches #"\d\-?\d{3}\-?\d{5}\-?X" isbn)
+              (= 0 (mod (+ 10 (reduce + (map * (range 10 1 -1) (isbn-x-str-to-digits isbn )))) 11))
               false)))
 
 (defn -main
